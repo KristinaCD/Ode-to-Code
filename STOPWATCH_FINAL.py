d84b2c8eeeda0340d576369584d373bb9d33b4b1
KristinaCD <105399269+KristinaCD@users.noreply.github.com>
2022-05-28 12:46:08 +0800

    Updated STOPWATCH_FINAL
    added commands and checking

--- STOPWATCH_FINAL.py
+++ STOPWATCH_FINAL.py
@@ -10,7 +10,7 @@ minutes = 0
 seconds = 0
 lap_number = 0
 
-
+# Functions
 def start():
     global running
     if not running:
@@ -76,6 +76,27 @@ root = Tk()
 root.title("Digital Clock/Stopwatch")
 root.geometry()
 
+# Date & Time (Not related to Stopwatch Project)
+def present_time():
+    display_time = time.strftime("%I:%M %p")
+    digi_clock.config(text=display_time)
+    digi_clock.after(1000,present_time)
+    day_name= time.strftime("(%A) %B %d, %Y")
+
+    my_label.config(text= day_name)
+    my_label2.config(text= "GMT+08:00 Philippine Standard Time")
+    
+Label(root,font=("Times New Roman",14),text="Date & Time",fg="black").pack()
+digi_clock = Label(root,font=("Times New Roman",35),fg="black")
+digi_clock.pack()
+
+my_label = Label(root,text="",font=("Arial",10))
+my_label.pack()
+my_label2 = Label(root,text="",font=("Arial",10))
+my_label2.pack()
+present_time()
+
+# Stopwatch Features
 Label(root,font=("Times New Roman",25,"bold"),text="Stopwatch",fg="black").pack(pady= 15)
 
 stopwatch_label=Label(root,text="00:00:00",fg="Black",font=("Times New Roman",40,"bold"))
@@ -84,16 +105,16 @@ stopwatch_label.pack()
 start_button=Button(root,text="Start",height=4,width=6,font=("Arial",10,"bold"),bg="Green",fg="Black", command= start)
 start_button.pack(side=LEFT)
 
-lap_button=Button(root,text="Lap",height=4,width=6,font=("Arial",10,"bold"),bg="Purple",fg="Black")
+lap_button=Button(root,text="Lap",height=4,width=6,font=("Arial",10,"bold"),bg="Purple",fg="Black", command= lap)
 lap_button.pack(side=LEFT)
 
-resume_button=Button(root,text="Resume",height=4,width=6,font=("Arial",10,"bold"),bg="Yellow green",fg="Black")
+resume_button=Button(root,text="Resume",height=4,width=6,font=("Arial",10,"bold"),bg="Yellow green",fg="Black", command= resume)
 resume_button.pack(side=LEFT)
         
-stop_button=Button(root,text="Stop",height=4,width=6,font=("Arial",10,"bold"),bg="Red",fg="Black")
+stop_button=Button(root,text="Stop",height=4,width=6,font=("Arial",10,"bold"),bg="Red",fg="Black", command= stop)
 stop_button.pack(side=LEFT)
 
-reset_button=Button(root,text="Reset",height=4,width=6,font=("Arial",10,"bold"),bg="Grey",fg="Black")
+reset_button=Button(root,text="Reset",height=4,width=6,font=("Arial",10,"bold"),bg="Grey",fg="Black", command= reset)
 reset_button.pack(side=LEFT)
 
 exit_button=Button(root,text="Exit",height=4,width=6,font=("Arial",10,"bold"),bg="White",fg="Black", command= quit)
